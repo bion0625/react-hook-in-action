cbe74c8972e0405d7973ade536264e0c89e8fea3
이의정 <stxtory@gmail.com>
2025-03-20 21:07:09 +0900

    2.2.5 새 상태를 설정할 때 이전 상태 참조하기

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,20 +1,32 @@
 import { useState } from "react";
 import data from "../../static.json"
+import { FaArrowRight } from "react-icons/fa";
 
 export default function BookablesList () {
 
     const group = "Rooms";
-
     const bookablesInGroup = data.bookables.filter(b => b.group === group);
     const [bookableIndex, setBookableIndex] = useState(1);
 
+    function nextBookable () {
+        setBookableIndex(i => (i + 1) % bookablesInGroup.length)
+    }
+
     return (
-        <ul className="bookables items-list-nav">
-            {bookablesInGroup.map((b, i) => (
-                <li key={b.id} className={i === bookableIndex ? "selected" : null}>
-                    <button className="btn" onClick={() => setBookableIndex(i)}>{b.title}</button>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <ul className="bookables items-list-nav">
+                {bookablesInGroup.map((b, i) => (
+                    <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+                        <button className="btn" onClick={() => setBookableIndex(i)}>{b.title}</button>
+                    </li>
+                ))}
+            </ul>
+            <p>
+                <button className="btn" onClick={nextBookable} autoFocus>
+                    <FaArrowRight/>
+                    <span>Next</span>
+                </button>
+            </p>
+        </div>
     )
 }
