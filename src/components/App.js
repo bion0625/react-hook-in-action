732ffe45e2250c57b61e73a02f29a82e537988f6
bion0625 <stxtory@gmail.com>
2025-03-20 13:13:58 +0900

    2.1 예약 관리 앱 설정하기

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import '../App.css';
+import { FaCalendarAlt, FaDoorOpen, FaUsers } from 'react-icons/fa';
+import UserPickers from './Users/UserPickers';
+import UsersPage from './Users/UsersPage';
+import BookingsPage from './Bookings/BookingsPage';
+import BookablesPage from './Bookables/BookablesPage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/bookings" className='btn btn-header'>
+                  <FaCalendarAlt />
+                  <span>Bookings</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/bookables" className='btn btn-header'>
+                  <FaDoorOpen />
+                  <span>Bookables</span>
+                </Link>
+              </li>
+              <li>
+                <Link to="/users" className='btn btn-header'>
+                  <FaUsers />
+                  <span>Users</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <UserPickers />
+        </header>
+
+        <Routes>
+          <Route path='/bookings' element={<BookingsPage />}></Route>
+          <Route path='/bookables' element={<BookablesPage />}></Route>
+          <Route path='/users' element={<UsersPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Users/UserPickers.js
@@ -0,0 +1,7 @@
+export default function UserPickers () {
+    return (
+        <select>
+            <option>Users</option>
+        </select>
+    )
+}
